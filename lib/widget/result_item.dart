add2583d69d73ea76ad14ec0ae147d4856780a03
Namhyun, Gu <mnhan0403@gmail.com>
2019-04-28 18:27:40 +0900

    Add share text content and show image content

--- lib/widget/result_item.dart
+++ lib/widget/result_item.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:preference_helper/preference_helper.dart';
+import 'package:share/share.dart';
+import 'package:sp_client/model/models.dart';
 import 'package:sp_client/model/result.dart';
+import 'package:sp_client/util/utils.dart';
 
 class ResultItem extends StatelessWidget {
   final int index;
@@ -14,17 +19,39 @@ class ResultItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isText = result.type == 'text';
+    String itemContent = result.content;
+    if (!isText) {
+      var serviceUrl = BlocProvider.of<PreferenceBloc>(context)
+          .getPreference<String>(AppPreferences.keyServiceUrl)
+          .value;
+      itemContent = serviceUrl + result.content;
+    }
+
     return ListTile(
-      leading: Text('#${index + 1}'),
-      title: (result.type == 'text'
-          ? Text(result.content)
-          : Image.network(result.content)),
-      onTap: () {
-        Clipboard.setData(ClipboardData(text: result.content));
-        Scaffold.of(context).showSnackBar(SnackBar(
-          content: Text(result.content),
-        ));
-      },
+      title: (isText ? Text(itemContent) : Image.network(itemContent)),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: (isText
+            ? <Widget>[
+                IconButton(
+                  icon: Icon(Icons.content_copy),
+                  onPressed: () {
+                    Clipboard.setData(ClipboardData(text: itemContent));
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text("Copied \"$itemContent\""),
+                    ));
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.share),
+                  onPressed: () {
+                    Share.share(itemContent);
+                  },
+                ),
+              ]
+            : []),
+      ),
     );
   }
 }
