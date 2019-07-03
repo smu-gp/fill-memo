cdd3b9f248c83a4b7c15c84d030d4683a3f4d4bf
NamHyun Gu <mnhan0403@gmail.com>
2019-07-03 11:47:33 +0900

    Add firebase performance, crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sp_client/app.dart';
@@ -9,10 +10,16 @@ import 'package:sqflite/sqflite.dart';
 void main() async {
   bool isProduction = bool.fromEnvironment('dart.vm.product');
   if (!isProduction) {
+    Crashlytics.instance.enableInDevMode = true;
     // ignore: deprecated_member_use
     await Sqflite.devSetDebugModeOn(true);
     BlocSupervisor.delegate = SimpleBlocDelegate();
   }
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    Crashlytics.instance.onError(details);
+  };
+
   final sharedPreferences = await SharedPreferences.getInstance();
   runApp(App(
     preferenceRepository: LocalPreferenceRepository(sharedPreferences),
